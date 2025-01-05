99d6f3b181afea18d9286db7fd00e2dca9394c43
Samuel Kendall <kinguser981@gmail.com>
2025-01-06 02:48:15 +0330

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -161,7 +161,7 @@ TW_INCLUDE_CRYPTO := true
 #TW_INCLUDE_CRYPTO_FBE := true
 TW_FORCE_KEYMASTER_VER := true
 BOARD_USES_METADATA_PARTITION := true
-BOARD_USES_QCOM_FBE_DECRYPTION := true
+#BOARD_USES_QCOM_FBE_DECRYPTION := true
 
 # to load touchscreen modules
 TW_LOAD_VENDOR_DLKM_MODULES := "tp_info.ko lct_tp.ko cmd.ko ilitek.ko icnl9922c.ko hx83112f.ko goodix_ts.ko atmel_mxt_ts.ko synaptics_tcm_ts.ko nt36xxx-i2c.ko pt_ts.ko pt_i2c.ko pt_device_access.ko raydium_ts.ko sdhci-msm-sec.ko" #
