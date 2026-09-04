ba6d7feedda8d25b7207906fa2c6a64c5756b0f8
nevadamurdock <nevada.murdock@gmail.com>
2026-09-05 05:14:21 +0700

    Add fox_a05s.mk to product makefiles and lunch choices

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,6 +1,14 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 TeamWin Recovery Project
+# Copyright (C) 2024 OrangeFox Recovery Project
+#
+
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/a05s.mk
+    $(LOCAL_DIR)/a05s.mk \
+    $(LOCAL_DIR)/fox_a05s.mk
 
 COMMON_LUNCH_CHOICES := \
     aosp_a05s-eng \
-    aosp_a05s-userdebug
+    aosp_a05s-userdebug \
+    fox_a05s-eng
