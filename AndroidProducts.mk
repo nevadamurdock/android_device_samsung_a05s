a096260e28110b971856131259b706b24cf9db18
nevadamurdock <nevada.murdock@gmail.com>
2026-09-18 02:00:53 +0700

    Add user build type to COMMON_LUNCH_CHOICES

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,13 @@
+#
+# Copyright (C) 2026 The LineageOS Project
+#
+# SP-License-Identifier: Apache-2.0
+#
+
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/aosp_a05s.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_a05s-user \
     aosp_a05s-userdebug \
+    aosp_a05s-user \
     aosp_a05s-eng
