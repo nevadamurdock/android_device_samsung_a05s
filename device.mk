c32b1fceb104e41f09ac3aeaf90494713760237c
nevadamurdock <nevada.murdock@gmail.com>
2026-09-18 01:59:58 +0700

    Update device.mk with vendor and permissions setup

--- device.mk
+++ device.mk
@@ -1,12 +1,33 @@
-# Call Vendor Blobs
-$(call inherit-product-if-exists, vendor/samsung/a05s/a05s-vendor.mk)
+#
+# Copyright (C) 2026 The LineageOS Project
+#
+# SP-License-Identifier: Apache-2.0
+#
 
-# Overlays
-DEVICE_PACKAGE_OVERLAYS += device/samsung/a05s/overlay
+LOCAL_PATH := device/samsung/a05s
 
-# Dynamic Partitions
+# Inherit Proprietary Vendor Blobs
+$(call inherit-product, vendor/samsung/a05s/a05s-vendor.mk)
+
+# Inherit Samsung Hardware HAL Extensions
+$(call inherit-product, hardware/samsung/hidl/samsung-hidl.mk)
+
+# Product Overlays
+PRODUCT_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+
+# Dynamic Partitions & System Modules
+PRODUCT_PACKAGES += \
+    vendor_boot \
+    android.hardware.audio.service \
+    android.hardware.graphics.composer-service \
+    android.hardware.bluetooth@1.0-service
+
+# Standard Permissions Setup
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.gsm.xml \
+    frameworks/native/data/etc/android.hardware.wifi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.xml \
+    frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
+    frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.accelerometer.xml
+
+# Enable Scoped Storage & Dynamic Partition Capabilities
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
-
-# Soong Namespaces
-PRODUCT_SOONG_NAMESPACES += \
-    device/samsung/a05s
