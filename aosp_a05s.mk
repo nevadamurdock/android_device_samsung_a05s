853d6efeae4e318691cbad84aef3eef2847a0f38
nevadamurdock <nevada.murdock@gmail.com>
2026-09-18 02:03:03 +0700

    Update branding information for aosp_a05s

--- aosp_a05s.mk
+++ aosp_a05s.mk
@@ -1,10 +1,20 @@
+#
+# Copyright (C) 2026 The LineageOS Project
+#
+# SP-License-Identifier: Apache-2.0
+#
+
+# Inherit AOSP Core Product Config
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit device configuration
+# Inherit Device Tree Utama
 $(call inherit-product, device/samsung/a05s/device.mk)
 
+# Identitas Produk Perangkat
 PRODUCT_NAME := aosp_a05s
 PRODUCT_DEVICE := a05s
-PRODUCT_BRAND := samsung
+PRODUCT_BRAND := Samsung
 PRODUCT_MODEL := SM-A057F
-PRODUCT_MANUFACTURER := samsung
+PRODUCT_MANUFACTURER := Samsung
+
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
