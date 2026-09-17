fefa56d50d5825e042c89d919f838cfa98d82c6c
nevadamurdock <nevada.murdock@gmail.com>
2026-09-18 01:53:46 +0700

    Update BoardConfig.mk for device a05s configuration

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,3 +1,11 @@
+#
+# Copyright (C) 2026 The LineageOS Project
+#
+# SP-License-Identifier: Apache-2.0
+#
+
+DEVICE_PATH := device/samsung/a05s
+
 # Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
@@ -6,48 +14,57 @@ TARGET_CPU_ABI2 :=
 TARGET_CPU_VARIANT := generic
 
 TARGET_2ND_ARCH := arm
-TARGET_2ND_ARCH_VARIANT := armv7-a-neon
+TARGET_2ND_ARCH_VARIANT := armv8-a
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := generic
-TARGET_SUPPORTS_64_BIT_APPS := true
 
-# Platform / Processor
-PRODUCT_PLATFORM := sm6225
-TARGET_BOARD_PLATFORM := sm6225
-TARGET_BOOTLOADER_BOARD_NAME := sm6225
+# OTA Assertions
+TARGET_OTA_ASSERT_DEVICE := a05s,a05sxx,SM-A057F
 
-# Kernel - Prebuilt Paths
-TARGET_PREBUILT_KERNEL := device/samsung/a05s/prebuilt/kernel
-BOARD_PREBUILT_DTBOIMAGE := device/samsung/a05s/prebuilt/dtbo.img
-BOARD_PREBUILT_DTBIMAGE_DIR := device/samsung/a05s/prebuilt/
-BOARD_INCLUDE_DTB_IN_BOOTIMG := true
-BOARD_MKBOOTIMG_ARGS += --dtb device/samsung/a05s/prebuilt/dtb
+# Kernel Build Configuration (Bengal 5.15 GKI)
+TARGET_KERNEL_ARCH := arm64
+TARGET_KERNEL_HEADER_ARCH := arm64
+TARGET_KERNEL_SOURCE := kernel/samsung/a05s
+TARGET_KERNEL_CONFIG := vendor/bengal_gki.config
+BOARD_KERNEL_IMAGE_NAME := Image
 
-# Kernel Header / Page Parameters
-BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 androidboot.usbcontroller=4e00000.mw3c swiotlb=0 loop.max_part=7
+# Boot Image Header Version 4 (GKI 5.15 Standard)
 BOARD_BOOT_HEADER_VERSION := 4
-BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
-
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_BASE := 0x00000000
-BOARD_KERNEL_OFFSET := 0x00008000
-BOARD_RAMDISK_OFFSET := 0x02000000
-BOARD_TAGS_OFFSET := 0x01e00000
+BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom androidboot.memcg=1 service_notifier.disable=1 androidboot.usbcontroller=4e00000.dwc3
+BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 
-# Kernel Modules Integration
-BOARD_VENDOR_KERNEL_MODULES := $(wildcard device/samsung/a05s/prebuilt/modules/*.ko)
-BOARD_VENDOR_RAMDISK_KERNEL_MODULES := $(wildcard device/samsung/a05s/prebuilt/modules/*.ko)
-
-# Partitions (Dynamic Partitions / EROFS)
-BOARD_SUPER_PARTITION_SIZE := 8589934592
+# Dynamic Partitions (Include Super & DLKM)
+BOARD_SUPER_PARTITION_SIZE := 9663676416
 BOARD_SUPER_PARTITION_GROUPS := samsung_dynamic_partitions
-BOARD_SAMSUNG_DYNAMIC_PARTITIONS_PARTITION_LIST := system system_ext product vendor vendor_dlkm system_dlkm
-BOARD_SAMSUNG_DYNAMIC_PARTITIONS_SIZE := 8585740288
+BOARD_SAMSUNG_DYNAMIC_PARTITIONS_SIZE := 9659482112
+BOARD_SAMSUNG_DYNAMIC_PARTITIONS_PARTITION_LIST := system vendor product system_ext vendor_dlkm system_dlkm
 
-# System / EROFS Configuration
-BOARD_HAS_LARGE_BLOCK_SIZE := true
-TARGET_USERIMAGES_USE_EROFS := true
+# Filesystem Image Types
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
+BOARD_SYSTEMIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_PRODUCTIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_SYSTEM_EXTIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_VENDOR_DLKMIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_SYSTEM_DLKMIMAGE_FILE_SYSTEM_TYPE := ext4
 
-# VINTF / Matrix
-DEVICE_MANIFEST_FILE := device/samsung/a05s/manifest.xml
+# Qualcomm Hardware & Display (CAF)
+BOARD_USES_QCOM_HARDWARE := true
+TARGET_USES_QCOM_BSP := true
+TARGET_USES_COLOR_METADATA := true
+TARGET_SCREEN_DENSITY := 270
+
+# VINTF Manifests
+DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/framework_compatibility_matrix.xml
+
+# SELinux (Permissive untuk pengujian awal)
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
+BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+
+# System Properties
+TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
